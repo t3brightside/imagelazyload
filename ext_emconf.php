9c65d357fde4eb22bca3cda9961b115c806b2c56
Tanel Põld <tanel@brightside.ee>
2025-02-01 14:17:50 +0200

    [TASK] 	fix versioning and compatibility in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => 'Image Lazy Load',
 	'description' => 'Images lazy load control for pagelist, personnel, gallerycontent, and custom elements.',
 	'category' => 'fe',
-	'version' => '1.0.1',
+	'version' => '1.1.1',
 	'state' => 'stable',
 	'clearcacheonload' => true,
 	'author' => 'Tanel Põld',
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
 	'author_company' => 'Brightside OÜ / t3brightside.com',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '11.5.0 - 12.99.99'
+			'typo3' => '12.4.0 - 13.99.99'
 		],
 	],
 ];
